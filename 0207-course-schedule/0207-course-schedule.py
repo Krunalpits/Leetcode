247a394642636d0c2e34d1b737b917b369ebbc27
Krunal Pithadia <125281938+Krunalpits@users.noreply.github.com>
2026-07-17 16:26:10 -0700

    Time: 7 ms (74.42%), Space: 16.4 MB (6.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,44 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        graph = {i: [] for i in range(numCourses)}
-        for courses, prereq in prerequisites:
-            graph[courses].append(prereq)
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        if there is cycle its impossible 
+        so find if unweighted graph is cyclic or acyclic.
 
-        state = [0] * numCourses
+        keep ana djacency list/hashmap => courses : prereqs list
+        one visited set to store courses thqt have been viisted. 
 
+        when you visit a course, check its prereqs from hashmap. visit prereq. add prereq also to visited.
+
+        if we visit a course thats already in the visisted set befoen that means cycle detected.
+        
+        """
+        hashmap={}
+        #populate premap to store all course and their prereqs
+        hashmap={i:[] for i in range(numCourses)}
+        for c,pre in prerequisites:
+            hashmap[c].append(pre)
+        visited=set()
         def dfs(course):
-            if state [course] == 1:
+            if course in visited:
                 return False
-            if state [course] == 2:
-                return True 
-
-            state [course] = 1
-
-            for prereq in graph[course]:
-                if not dfs(prereq):
-                    return False
-
-            state[course] = 2
+            if hashmap[course]==[]:
+                return True
+            visited.add(course)
+            #explore prereqs of visited node
+            for prereq in hashmap[course]:
+                result=dfs(prereq)
+                 #if no cycle for prereq->true->can finish allcourses
+                if not result:
+                    return False #cycle detected for prereq
+            visited.remove(course)
+            hashmap[course]=[]
             return True
 
-        for course in range(numCourses):
+        for course,pre in prerequisites:
             if not dfs(course):
                 return False
+        return True   
 
-        return True
